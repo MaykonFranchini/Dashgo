faac366b335d7ac7de98609dd5b679528dae1240
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-09-07 04:46:28 +0100

    Login page with form created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 export default function Home() {
   return (
-        <h1>Dashgo</h1>
+    <Flex 
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+        >
+      <Flex 
+        as="form" 
+        width="100%" 
+        maxWidth={360} 
+        backgroundColor="gray.800" 
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+          
+
+          <Button type="submit" marginTop="6" colorScheme="pink" size="lg">Entrar</Button>
+      
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
