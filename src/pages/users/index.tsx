235543da9a61685aaafcf4d17c05c0348f5ee760
MaykonFranchini <maykonfranchini2017@gmail.com>
2021-09-08 04:00:29 +0100

    Pagination coomponent added

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,8 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Thead, Th, Tr, Tbody
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar'
+import { Pagination } from '../../components/Pagination';
+
 export default function UserList() {
   return (
     <Box>
@@ -83,7 +85,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
-
+          <Pagination />
 
         </Box>
       </Flex>
