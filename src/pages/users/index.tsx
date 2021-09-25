6630f32e5a3d416f90e8c741262dbdaac0ed78bb
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-09-25 23:26:00 +0100

    react query config + data fectching done

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,39 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Thead, Th, Tr, Tbody, Td, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Thead, Th, Tr, Tbody, Td, Text, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header/Index';
 import { Sidebar } from '../../components/Sidebar/Index'
 import { Pagination } from '../../components/Pagination/Index';
 import Link from 'next/link'
+import { useQuery } from 'react-query';
+
 
 export default function UserList() {
+
+  const {data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR')
+      };
+    })
+
+    return users;
+  }, {
+    staleTime: 1000 * 5 // -- 5 seg -- determina o tempo em que os dados permanecem fresh, ou seja, sem necessidade de refecth.
+  })
+
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
+
 
   return (
     <Box>
@@ -35,69 +58,60 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha" >
-            <Thead>
-              <Tr>
-                <Th paddingX={["4","4","6"]} color="gray.300" width={["6","8"]} > 
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td paddingX={["4","4","6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Maykon Franchini</Text>
-                    <Text fontSize="sm" color="gray.300">maykonfranchini2017@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>08 de Setembro de 2021</Td> }
-                <Td>
-                  <Button 
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    iconSpacing={isWideVersion ? '1.5': '-0.5'}
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    {isWideVersion && 'Editar'}
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td paddingX={["4","4","6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Paola Franchini</Text>
-                    <Text fontSize="sm" color="gray.300">paolafranchini2017@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>07 de Setembro de 2021</Td> }
-                <Td>
-                  <Button 
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    iconSpacing={isWideVersion ? '1.5': '-0.5'}
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    {isWideVersion && 'Editar'}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) :  error ? (
+            <Flex justify="center" ><Text>Erro ao carregar os dados</Text></Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha" >
+              <Thead>
+                <Tr>
+                  <Th paddingX={["4","4","6"]} color="gray.300" width={["6","8"]} > 
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  { isWideVersion && <Th>Data de cadastro</Th>}
+                  <Th width="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                
+                {data.map(user => {
+                  return (
+                    <Tr key={user.id}>
+                  <Td paddingX={["4","4","6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">{user.name}</Text>
+                      <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                    </Box>
+                  </Td>
+                  { isWideVersion && <Td>{user.createdAt}</Td> }
+                  <Td>
+                    <Button 
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      iconSpacing={isWideVersion ? '1.5': '-0.5'}
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      {isWideVersion && 'Editar'}
+                    </Button>
+                  </Td>
+                </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
+            <Pagination />
+          </>
+          )}
 
         </Box>
       </Flex>
