62e54f282584145709ed93fa17a40a0a1af5b3cf
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-09-27 15:50:46 +0100

    hook to fetch user

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,23 +1,31 @@
 import { useQuery } from "react-query";
 import { api } from "../api";
 
+type Users = {
+    id: number;
+    name: string;
+    email: string;
+    createdAt: string;
+}
+
+export async function getUsers(): Promise<Users[]> {
+    const { data } = await api.get('/users')
+    
+    const users = data.users.map(user => {
+        return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR')
+        };
+    })
+
+    return users;
+}
 
 export function useUsers() {
 
-   return useQuery('users', async () => {
-        const { data } = await api.get('/users')
-        
-        const users = data.users.map(user => {
-          return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR')
-          };
-        })
-    
-        return users;
-      }, {
+   return useQuery('users', getUsers, {
         staleTime: 1000 * 5 // -- 5 seg -- determina o tempo em que os dados permanecem fresh, ou seja, sem necessidade de refecth.
       })
 
